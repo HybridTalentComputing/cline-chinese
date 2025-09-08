b27c21bd0ca2f24832b0bb25520fca1f88b5a978
shengsuan-cloud <developer@modelmesh.io>
2025-09-08 10:42:51 +0800

    ...

--- webview-ui/src/components/settings/sections/GeneralSettingsSection.tsx
+++ webview-ui/src/components/settings/sections/GeneralSettingsSection.tsx
@@ -1,5 +1,4 @@
-import { VSCodeCheckbox, VSCodeLink } from "@vscode/webview-ui-toolkit/react"
-import { useExtensionState } from "@/context/ExtensionStateContext"
+import { VSCodeLink } from "@vscode/webview-ui-toolkit/react"
 import PreferredLanguageSetting from "../PreferredLanguageSetting"
 import Section from "../Section"
 
@@ -8,21 +7,19 @@ interface GeneralSettingsSectionProps {
 }
 
 const GeneralSettingsSection = ({ renderSectionHeader }: GeneralSettingsSectionProps) => {
-	const { telemetrySetting } = useExtensionState()
-
 	return (
 		<div>
 			{renderSectionHeader("general")}
 			<Section>
 				<PreferredLanguageSetting />
 				<div className="mb-[5px]">
-					<h2 className="block mb-1 text-sm font-medium">本插件不会搜集和传输任何用户信息</h2>
+					<h2 className="block mb-1 text-sm font-medium">本插件不会收集或传输任何用户信息</h2>
 					<p className="text-xs mt-[5px] text-[var(--vscode-descriptionForeground)]">
-						选择使用胜算云 API Router 功能的用户。请参阅我们的{" "}
+						选择使用胜算云 API 的用户。请参阅{" "}
 						<VSCodeLink className="text-inherit" href="https://docs.router.shengsuanyun.com/terms-of-service">
 							使用条款
 						</VSCodeLink>{" "}
-						and{" "}
+						和{" "}
 						<VSCodeLink className="text-inherit" href="https://docs.router.shengsuanyun.com/privacy-policy">
 							隐私政策
 						</VSCodeLink>{" "}
