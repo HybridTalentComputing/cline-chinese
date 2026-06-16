dce894649857f2313642f1d06d2edda4e07ffd45
HybridTalentComputing <1316645276@qq.com>
2026-06-16 22:04:59 +0800

    fix(auth): remove unnecessary 'from' parameter from auth URL

--- src/core/controller/account/shengSuanYunLoginClicked.ts
+++ src/core/controller/account/shengSuanYunLoginClicked.ts
@@ -6,7 +6,6 @@ import { Controller } from ".."
 export async function shengSuanYunLoginClicked(_: Controller, __: EmptyRequest): Promise<Empty> {
 	const callbackUrl = await HostProvider.get().getCallbackUrl("/ssy")
 	const authUrl = new URL("https://router.shengsuanyun.com/auth")
-	authUrl.searchParams.set("from", "cline-chinese")
 	authUrl.searchParams.set("callback_url", callbackUrl)
 
 	await openExternal(authUrl.toString())
